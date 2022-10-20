ee8e7baf115b00742d568b6d51c91a6c55541160
XiongSiMiao <1712650054@qq.com>
2022-10-20 18:18:45 +0800

    Initial commit

--- src/JungleModel/Board.java
+++ src/JungleModel/Board.java
@@ -2,4 +2,12 @@ package JungleModel;
 
 public class Board {
     public Board(){}
+
+
+    public void setPiece(Piece p){}
+
+
+    private class River{}
+    private class Den{}
+    private class Trap{}
 }
