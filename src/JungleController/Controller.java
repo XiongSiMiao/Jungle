49a306b1604f8dc4aa367ea2b96b15511487d501
XiongSiMiao <1712650054@qq.com>
2022-10-20 15:07:21 +0800

    Initial commit

--- src/JungleController/Controller.java
+++ src/JungleController/Controller.java
@@ -1,9 +1,18 @@
 package JungleController;
-
+import JungleModel.Board;
+import Exception.WrongMovement;
+import Exception.LosePieces;
 public class Controller {
     public Controller(){
     }
 
     public static void start() {
+    }
+
+    public static void movement(String dir){}
+
+    public static enum Owner{
+        user1,user2;
+        private Owner(){}
     }
 }
