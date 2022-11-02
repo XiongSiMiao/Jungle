dd61e6d1ae18b6cc53118b31955552bfc3913485
XiongSiMiao <1712650054@qq.com>
2022-11-02 20:25:43 +0800

    Initial commit

--- src/test/model/pieceType/LeopardTest.java
+++ src/test/model/pieceType/LeopardTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CatTest {
+class LeopardTest {
 
     @Test
     void setPiece() {
--- src/test/model/pieceType/TigerTest.java
+++ src/test/model/pieceType/TigerTest.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class CatTest {
+class TigerTest {
 
     @Test
     void setPiece() {
