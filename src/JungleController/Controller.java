a87433b85287667f204195bd13f445a53191beba
XiongSiMiao <1712650054@qq.com>
2022-11-14 17:39:19 +0800

    Initial commit

--- src/JungleController/Controller.java
+++ src/JungleController/Controller.java
@@ -14,7 +14,25 @@ public class Controller {
     
 
     public static void replace(Piece p) throws LosePieces{}//replace the opponent's piece
-    public static void movement(Piece p,String dir) throws WrongMovement{}//change p.locationX and p.locationY
+    public static void movement(Piece p,String dir) throws WrongMovement{
+        switch (dir){
+            /*
+            缺少一个边界检查
+             */
+            case "w":
+                p.locationY=p.locationY+1;
+                break;
+            case "a":
+                p.locationX=p.locationX-1;
+                break;
+            case "s":
+                p.locationY=p.locationY-1;
+                break;
+            case "d":
+                p.locationX=p.locationX+1;
+                break;
+        }
+    }//change p.locationX and p.locationY
 
     public boolean moveChecker(Piece p, String dir) {
         return true;
